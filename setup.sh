90ba8eaf6a960b6b793a4cd395c02cc563a41875
jkovacevic <jk.simunic@gmail.com>
2018-06-04 01:56:05 +0200

    added gitconfig

--- setup.sh
+++ setup.sh
@@ -11,17 +11,18 @@ else
 fi;
 echo "- i3wm"
 rm $HOME/.config/i3/config; ln -s $HOME/dotfiles/i3-$os.md $HOME/.config/i3/config;
-
+echo "- git"
+rm $HOME/.gitconfig; ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
 echo "- ipython"
 rm $HOME/.ipython/profile_default/ipython_config.py; ln -s $HOME/dotfiles/ipython.py $HOME/.ipython/profile_default/ipython_config.py;
-
 echo "- sublime"
-rm $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings; ln -s $HOME/dotfiles/sublime-settings.json $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings;
-rm $HOME/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap; ln -s $HOME/dotfiles/sublime-keymap.json $HOME/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap;
-rm $HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings; ln -s $HOME/dotfiles/sublime-lhistory.json $HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings;
-
+rm $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings; 
+ln -s $HOME/dotfiles/sublime-settings.json $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings;
+rm $HOME/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap; 
+ln -s $HOME/dotfiles/sublime-keymap.json $HOME/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap;
+rm $HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings; 
+ln -s $HOME/dotfiles/sublime-lhistory.json $HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings;
 echo "- termite"
 rm $HOME/.config/termite/config; ln -s $HOME/dotfiles/termite.sh $HOME/.config/termite/config;
-
 echo "- zshrc"
 rm $HOME/.zshrc; ln -s $HOME/dotfiles/zshrc-$os.sh $HOME/.zshrc
