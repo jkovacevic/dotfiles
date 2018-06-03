b4ce2cfb96b58a49d014c2a756e5161845ec9223
jkovacevic <jk.simunic@gmail.com>
2018-06-04 01:51:27 +0200

    modified alias for git pull and push

--- zshrc-arch.sh
+++ zshrc-arch.sh
@@ -61,8 +61,8 @@ wss() {sudo grep -rn $1; }
 
 # Git shortcuts
 ga () {	if [[ $1 == "" ]]; then git add .;	else git add $1; fi }
-gpu () {git push}
-gpl () {git pull}
+gp () {git push}
+gf () {git pull}
 gs () {git status}
 gc () {git commit -m "$1"}
 gr () {git reset HEAD^}
