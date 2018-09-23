995344ad186815a1438b341768428018f1503593
jkovacevic <jk.simunic@gmail.com>
2018-09-23 18:32:40 +0200

    no-commit-message

--- bash/create_ss/ss.sh
+++ bash/create_ss/ss.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+find_image_name() {
+	IMAGE_NAME="/tmp/img1.png"
+	while [ -f $IMAGE_NAME ]
+	do
+		n=$(echo $IMAGE_NAME | sed -e 's#/tmp/img\(.*\).png#\1#')
+		n=$((n + 1))
+		IMAGE_NAME="/tmp/img$n.png"
+	done
+}
 
-x="/tmp/img1.png" && touch $x && scrot -s $x;
-echo -n "/tmp/img1.png" | xclip -selection clipboard;
+find_image_name
+x=$IMAGE_NAME && touch $x && scrot -s $x;
+echo -n $IMAGE_NAME | xclip -selection clipboard;
