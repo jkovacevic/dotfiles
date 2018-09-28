97f2cd10fceefeeba585ff443ad927d7cd52b5d6
jkovacevic <jk.simunic@gmail.com>
2018-09-29 00:58:50 +0200

    automated commit message

--- bash/dmenu-search/dmenu_search.sh
+++ bash/dmenu-search/dmenu_search.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-xsel -o | dmenu -fn "DejaVu Sans-12" -p "Google:" | xargs -I {} google-chrome-stable https://www.google.com/search\?q={}
+xsel -c
+x=$(xsel -o | dmenu -fn "DejaVu Sans-12" -p "Google:" | xargs -r -I {} google-chrome-stable https://www.google.com/search\?q={} | wc -l)
+if [ $x -eq 1 ]; then
+	exec i3-msg [class="Google-chrome"] focus
+fi;
