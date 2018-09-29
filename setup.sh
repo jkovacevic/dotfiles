da290ca1b0160adb62bcebc2e919e25ee0dd5349
jkovacevic <jk.simunic@gmail.com>
2018-09-29 11:17:29 +0200

    automated commit message

--- setup.sh
+++ setup.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
+
+function create_symlink {
+	dotfile="$1"
+	target="$2"
+	echo "Creating symlink $target -> $dotfile"
+	if [ -f "$target" ] || [ -L "$target" ]; then
+		sudo rm "$target"
+	fi;
+
+	sudo ln -s "$dotfile" "$target"
+}
+
 echo "## Setup dotfiles ##"
-
-echo "Configuration"
-echo "- i3wm"
-rm $HOME/.config/i3/config; ln -s $HOME/dotfiles/i3.md $HOME/.config/i3/config;
-echo "- ipython"
-rm $HOME/.ipython/profile_default/ipython_config.py; ln -s $HOME/dotfiles/ipython.py $HOME/.ipython/profile_default/ipython_config.py;
-echo "- sublime"
-rm $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings; 
-ln -s $HOME/dotfiles/sublime-settings.json $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings;
-rm $HOME/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap; 
-ln -s $HOME/dotfiles/sublime-keymap.json $HOME/.config/sublime-text-3/Packages/User/Default\ \(Linux\).sublime-keymap;
-rm $HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings; 
-ln -s $HOME/dotfiles/sublime-history.json $HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings;
-echo "- urxvt"
-rm $HOME/.Xdefaults; ln -s $HOME/dotfiles/Xdefaults $HOME/.Xdefaults;
-echo "- zshrc"
-rm $HOME/.zshrc; echo "source $HOME/dotfiles/zshrc.sh" >> $HOME/.zshrc; 
-
-echo "Scripts"
-echo "- ss"
-sudo ln -s $HOME/dotfiles/bash/create_ss/ss.sh /usr/bin/ss
-echo "- dmenu search"
-sudo ln -s $HOME/dotfiles/dmenu-search/dmenu_search.sh /usr/bin/dmenu_search
+create_symlink "$HOME/dotfiles/zshrc.sh" 						"$HOME/.zshrc"
+create_symlink "$HOME/dotfiles/i3.md" 							"$HOME/.config/i3/config"
+create_symlink "$HOME/dotfiles/ipython.py" 						"$HOME/.ipython/profile_default/ipython_config.py"
+create_symlink "$HOME/dotfiles/sublime-settings.json" 			"$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+create_symlink "$HOME/dotfiles/sublime-keymap.json" 			"$HOME/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
+create_symlink "$HOME/dotfiles/sublime-history.json" 			"$HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings"
+create_symlink "$HOME/dotfiles/Xdefaults" 						"$HOME/.Xdefaults"
+create_symlink "$HOME/dotfiles/perl/keyboard-select"  			"/usr/lib/urxvt/perl/keyboard-select"
+create_symlink "$HOME/dotfiles/bash/create_ss/ss.sh" 			"/usr/bin/ss"
+create_symlink "$HOME/dotfiles/dmenu-search/dmenu_search.sh"	"/usr/bin/dmenu_search"
