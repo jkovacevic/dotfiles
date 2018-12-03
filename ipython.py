228a7e0731d3e8410dc1cdaa66a053ae8cc983a6
jkovacevic <janko.kovacevic@smaato.com>
2018-12-03 13:57:44 +0100

    automated commit message

--- ipython.py
+++ ipython.py
@@ -1,18 +1,5 @@
-try:
-	import pandas as pd
-	pd.set_option('display.max_rows', 0)
-	pd.set_option('display.max_columns', 0)
-except:
-	pass
-
-try:	
-	import numpy as np
-	np.core.arrayprint._line_width = 120
-	np.set_printoptions(formatter={'float_kind': lambda x: "%.2f" % x})
-except:
-	pass
-
-c.TerminalInteractiveShell.highlighting_style = 'igor'
+c.TerminalInteractiveShell.highlighting_style = 'monokai'
+#c.TerminalInteractiveShell.highlighting_style = 'igor'
 c.TerminalInteractiveShell.highlight_matching_brackets = False
 
 # Configuration file for ipython.
