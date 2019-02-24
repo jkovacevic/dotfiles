a8f46946eb4506a48b455b286fa83ac5ccd9e4c5
jkovacevic <jk.simunic@gmail.com>
2019-02-24 23:29:29 +0100

    automated commit message

--- ipython.py
+++ ipython.py
@@ -1,3 +1,18 @@
+
+try:
+	import pandas as pd
+	pd.set_option('display.max_rows', 0)
+	pd.set_option('display.max_columns', 0)
+except:
+	pass
+
+try:	
+	import numpy as np
+	np.core.arrayprint._line_width = 120
+	np.set_printoptions(formatter={'float_kind': lambda x: "%.2f" % x})
+except:
+	pass
+
 c.TerminalInteractiveShell.highlighting_style = 'monokai'
 c.TerminalInteractiveShell.highlight_matching_brackets = False
 
