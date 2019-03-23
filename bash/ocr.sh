ebebbcda7c8fb0cc2648dc1a276fe1f67967a9ba
jkovacevic <jk.simunic@gmail.com>
2019-03-23 14:43:37 +0100

    automated commit message

---
+++ bash/ocr.sh
@@ -0,0 +1,8 @@
+FILE_NAME=/tmp/_1
+FILE_PATH=/tmp/_1.txt
+FILE_PATH2=/tmp/_2.txt
+/usr/local/bin/screenshot
+tesseract -l eng --dpi 200 $(xclip -selection clipboard -o) $FILE_NAME
+sed '/^[[:space:]]*$/d' $FILE_PATH > $FILE_PATH2
+cat $FILE_PATH2 | xclip -selection clipboard
+notify-send "OCR copied to clipboard"
--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ create_symlink "$HOME/dotfiles/themes/arc-red-dark.rasi" "$HOME/.config/rofi/arc
 create_symlink "$HOME/dotfiles/Xdefaults" "$HOME/.Xdefaults"
 create_symlink "$HOME/dotfiles/bash/screenshot.sh" "/usr/local/bin/screenshot"
 create_symlink "$HOME/dotfiles/bash/video.sh" "/usr/local/bin/video"
+create_symlink "$HOME/dotfiles/bash/ocr.sh" "/usr/local/bin/ocr"
 create_symlink "$HOME/dotfiles/bash/sxiv-key.sh" "$HOME/.config/sxiv/exec/key-handler"
 create_symlink "$HOME/dotfiles/python/ipython.py" "$HOME/.ipython/profile_default/ipython_config.py"
 create_symlink "$HOME/dotfiles/python/file_format.py" "/usr/local/bin/file_format.py"
@@ -29,3 +30,4 @@ create_symlink "$HOME/dotfiles/sublime/sublime-keymap.json" "$HOME/.config/subli
 create_symlink "$HOME/dotfiles/sublime/sublime-history.json" "$HOME/.config/sublime-text-3/Packages/User/LocalHistory.sublime-settings"
 create_symlink "$HOME/dotfiles/applications/screenshot.desktop" "$HOME/.local/share/applications/screenshot.desktop"
 create_symlink "$HOME/dotfiles/applications/video.desktop" "$HOME/.local/share/applications/video.desktop"
+create_symlink "$HOME/dotfiles/applications/ocr.desktop" "$HOME/.local/share/applications/ocr.desktop"
