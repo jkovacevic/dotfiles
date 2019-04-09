3fef4061999320f4a4be6308e6ff863271295a1f
jkovacevic <janko.kovacevic@smaato.com>
2019-04-09 13:16:10 +0200

    automated commit message

--- bash/discard.sh
+++ bash/discard.sh
@@ -5,3 +5,6 @@ discard_file_path=$discard_folder/$1.txt
 
 if [ -f $discard_file_path ]; then echo "File with the same name ($discard_file_path) already exists!"; exit 1; fi;
 cp $tmp_file $discard_file_path
+echo "Moved $tmp_file to $discard_file_path"
+rm $tmp_file; touch $tmp_file
+echo "File $tmp_file had been cleaned"
