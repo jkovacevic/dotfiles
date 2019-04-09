8f6ef765f046a8adba7b0b7e0bd09dfe15d350b6
jkovacevic <janko.kovacevic@smaato.com>
2019-04-09 11:06:19 +0200

    automated commit message

---
+++ bash/discard.sh
@@ -0,0 +1,7 @@
+if [ "$#" -ne 1 ]; then echo "Missing CLI argument of discarded file name."; fi;
+tmp_file=$HOME/notes/tmp.txt
+discard_folder=$HOME/notes/discard
+discard_file_path=$discard_folder/$1.txt
+
+if [ -f $discard_file_path ]; then echo "File with the same name ($discard_file_path) already exists!"; exit 1; fi;
+cp $tmp_file $discard_file_path
