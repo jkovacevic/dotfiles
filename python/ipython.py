e646c8e19bcac25c4af6159c949b09ba387f6d18
jkovacevic <janko.kovacevic@smaato.com>
2019-06-11 10:03:33 +0200

    automated commit message

--- python/ipython.py
+++ python/ipython.py
@@ -8,9 +8,11 @@ except:
 try:	
 	import numpy as np
 	np.core.arrayprint._line_width = 120
-	np.set_printoptions(formatter={'float_kind': lambda x: "%.2f" % x})
+	np.set_printoptions(formatter={'float_kind': lambda x: "%.4if" % x})
 except:
 	pass
 
+c.InteractiveShell.color_info = True
+c.InteractiveShell.colors = 'Linux'
 c.TerminalInteractiveShell.highlighting_style = 'monokai'
-c.TerminalInteractiveShell.highlight_matching_brackets = False
+c.TerminalInteractiveShell.highlight_matching_brackets = True
