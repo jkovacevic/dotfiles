7ba68bbb80312628864767a855c7d3ade094deb9
jkovacevic <janko.kovacevic@smaato.com>
2019-07-01 10:16:48 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -5,17 +5,16 @@ cppsh() { xclip -selection clipboard -o > $1; }
 cpth() { readlink -f $1 | xargs echo -n | xclip -selection clipboard; }
 evince () { bg evince "$@" }
 fd() { eval subl --command \'sbs_compare_files {\"A\":\"$(realpath $1)\", \"B\":\"$(realpath $2)\"}\'; }
-ws() { rgrep -rn $1 --color=auto; }
 fs() {find -iname \*$1\*; }
+fss() {sudo find -iname \*$1\*; }
 libreoffice () { bg libreoffice "$@" }
-thunar () { bg thunar "$@" }
 pinta () { bg pinta "$@" }
 sxiv () { if [[ $# -eq '0' ]]; then bg /usr/bin/sxiv -t -a .; elif [[ -d $1 ]]; then bg /usr/bin/sxiv -t -a $1; else bg /usr/bin/sxiv -a $@; fi; }
 vlc() { bg vlc "$@" }
+ws() { rgrep -rn $1 --color=auto; }
+wss() { sudo rgrep -rn $1 --color=auto; }
 ymp3() { youtube-dl --extract-audio --audio-format mp3 $1; }
 yvid() { youtube-dl $1; }
-
-avenv() { source venv/bin/activate }
 
 # Git shortcuts
 gg () {git add .; git commit -m "automated commit message"; git push;}
@@ -46,6 +45,14 @@ push_to_branch() {
 	git commit -m "automated commit message";
 	git push;
 	git checkout $local_branch;
+}
+
+aws() {	if [[ "$#" -eq 3 && "$2" == "tree" ]]; then aws_s3_tree $@; else command aws $@; fi; }
+aws_s3_tree() {
+	KEY=$3
+	BUCKET=$(grep -oP "(?<=s3://).*?(?=/)" <<< "$KEY")
+	PREFIX=$(sed "s/.*$BUCKET\///" <<< "$KEY")
+	aws s3api list-objects --bucket $BUCKET --prefix $PREFIX | jq ".Contents[] .Key" | sed s/\"//g | sed -e s#$PREFIX##
 }
 
 copy_cmd() { 
