ed901b1514ebaa00f957e1da8799e63b639c4a44
jkovacevic <jk.simunic@gmail.com>
2019-07-06 00:48:35 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -27,12 +27,12 @@ gr () { git checkout $1 }
 gh () { smerge log $1 }
 gc () { git commit -m "$1" }
 gb () { git pull; git branch -a }
-gbl () { git branch | fzf | xargs git checkout }
-gbr () { git branch -r | grep -v HEAD | fzf | xargs git checkout -t }
+gbl () { git branch | fzf --prompt='local-branch > ' | xargs git checkout }
+gbr () { git branch -r | grep -v HEAD | fzf --prompt='origin-branch > ' | xargs git checkout -t }
 
 gdb() {
 	if [ "$#" -ne 1 ]; then echo "Expected arguments: file_name"; return 1; fi;
-	remote_branch=$(git branch -r | grep -v HEAD | fzf | xargs)
+	remote_branch=$(git branch -r | grep -v HEAD | fzf --prompt='origin-branch > ' | xargs)
 	local_path=/tmp/$(basename $remote_branch)-$(basename $1)
 	git cat-file blob $remote_branch:$1 > $local_path;
 	fd $1 $local_path;
@@ -41,7 +41,7 @@ gdb() {
 gptb() {
 	if [ "$#" -ne 1 ]; then echo "Expected arguments: file_name"; return 1; fi;
 	file_name=$1;
-	remote_branch=$(git branch | awk '{print $NF}' | fzf);
+	remote_branch=$(git branch | awk '{print $NF}' | fzf --prompt='origin-branch > ');
 	local_branch=$(git symbolic-ref --short HEAD)
 	cpcat $file_name;
 	git checkout $remote_branch;
@@ -76,3 +76,17 @@ copy_cmd() {
 	zle kill-buffer; 
 	print -rn -- $CUTBUFFER | xclip -selection clipboard; 
 }; zle -N copy_cmd
+
+man-find() {
+	man -k . | fzf --prompt='man-pages > ' | awk '{print $1}' | xargs -r man
+}
+
+pkill() {
+  	local pid 
+	if [ "$UID" != "0" ]; then
+	    pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+	else
+	    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+	fi;
+	if [ "x$pid" != "x" ]; then echo $pid | xargs kill -${1:-9}; fi
+}
