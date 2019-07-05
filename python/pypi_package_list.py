67a5b56d4ddb4b3b121568e63650821c72429b6b
jkovacevic <jk.simunic@gmail.com>
2019-07-06 01:39:08 +0200

    automated commit message

---
+++ python/pypi_package_list.py
@@ -0,0 +1,7 @@
+from lxml import html
+import requests
+response = requests.get("https://pypi.org/simple/")
+tree = html.fromstring(response.content)
+package_list = [package for package in tree.xpath('//a/text()')]
+for package in package_list:
+	print(package)
