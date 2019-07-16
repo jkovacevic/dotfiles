34f02ee25f5c9ade8a91a3870a1f1d2b0c9ec571
jkovacevic <jk.simunic@gmail.com>
2019-07-16 13:01:23 +0200

    automated commit message

--- bash/scrap.sh
+++ bash/scrap.sh
@@ -1,10 +1,18 @@
-if [ "$#" -ne 1 ]; then echo "Missing CLI argument of scrapped file name."; fi;
+log() {
+	echo "$1" && notify-send "$1"
+}
+
+if [ "$#" -ne 1 ]; then log "Missing CLI argument of scrapped file name."; fi;
 tmp_file=$HOME/notes/tmp.txt
 scrap_folder=$HOME/notes/scrap
 scrap_file_path=$scrap_folder/$1
 
-if [ -f $scrap_file_path ]; then echo "File with the same name ($scrap_file_path) already exists!"; exit 1; fi;
+if [ -f $scrap_file_path ]; then 
+	log "File with the same name ($scrap_file_path) already exists!";
+	exit 1; 
+fi;
+
 cp $tmp_file $scrap_file_path
-echo "Moved $tmp_file to $scrap_file_path"
+log "Moved $tmp_file to $scrap_file_path"
 rm $tmp_file; touch $tmp_file
-echo "File $tmp_file had been cleaned"
+log "File $tmp_file had been cleaned"
