471c8ace150f974ed3e4d1a0fa977756e44d43b4
jkovacevic <janko.kovacevic@smaato.com>
2019-08-05 10:17:34 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -10,11 +10,11 @@ fd() { eval subl --command \'sbs_compare_files {\"A\":\"$(realpath $1)\", \"B\":
 cpcat() { cat $1 | xclip -selection clipboard; }
 cppsh() { xclip -selection clipboard -o > $1; }
 cpth() { readlink -f $1 | xargs echo -n | xclip -selection clipboard; }
-ws() { rgrep -rn $1 --color=auto; }
-wss() { sudo rgrep -rn $1 --color=auto; }
+ws() { rg -n $@ --color=auto; }
+wss() { sudo rg -n $@ --color=auto; }
 ymp3() { youtube-dl --extract-audio --audio-format mp3 $1; }
 yvid() { youtube-dl $1; }
-avenv() { source venv/bin/activate; }
+venv() { if [[ "$VIRTUAL_ENV" == "" ]]; then source venv/bin/activate; else deactivate; fi; }
 
 # Git functions
 gg () { git add .; git commit -m "automated commit message"; git push; }
@@ -27,35 +27,39 @@ gr () { git checkout $1 }
 gh () { smerge log $1 }
 gc () { git commit -m "$1" }
 gb () { git pull; git branch -a }
-glb () { git branch | fzf --prompt='local-branch > ' | xargs git checkout }
-grb () { git branch -r | grep -v HEAD | fzf --prompt='origin-branch > ' | xargs git checkout -t }
+gbc () { 
+    local branch=$(git branch -a | grep -v HEAD | fzf --prompt='checkout-branch > ' | awk '{print $NF}';)
+    if [[ $branch == *"remotes/origin"* ]]; then git checkout -t $branch; else git checkout $branch; fi;  
+}
 
-gdb() {
-	if [ "$#" -ne 1 ]; then echo "Expected arguments: file_name"; return 1; fi;
-	remote_branch=$(git branch -r | grep -v HEAD | fzf --prompt='origin-branch > ' | xargs)
-	local_path=/tmp/$(basename $remote_branch)-$(basename $1)
-	git cat-file blob $remote_branch:$1 > $local_path;
-	fd $1 $local_path;
+gbd() {
+
+    if [ "$#" -ne 1 ]; then echo "Usage: gbd file.txt"; return 1; fi;
+    remote_branch=$(git branch -r | grep -v HEAD | fzf --prompt='origin-branch > ' | xargs)
+    local_path=/tmp/$(basename $remote_branch)-$(basename $1)
+    git cat-file blob $remote_branch:$1 > $local_path;
+    fd $1 $local_path;
 }
 
 gptb() {
-	if [ "$#" -ne 1 ]; then echo "Expected arguments: file_name"; return 1; fi;
-	file_name=$1;
-	local_branch=$(git branch | awk '{print $NF}' | fzf --prompt='local-branch > ');
-	current_branch=$(git symbolic-ref --short HEAD)
-	cpcat $file_name;
-	git checkout $local_branch;
-	cppsh $file_name;
-	git add $file_name;
-	git commit -m "automated commit message";
-	git push;
-	git checkout $current_branch;
+
+    if [ "$#" -ne 1 ]; then echo "Usage: gptb file.txt"; return 1; fi;
+    file_name=$1;
+    local_branch=$(git branch | awk '{print $NF}' | fzf --prompt='local-branch > ');
+    current_branch=$(git symbolic-ref --short HEAD)
+    cpcat $file_name;
+    git checkout $local_branch;
+    cppsh $file_name;
+    git add $file_name;
+    git commit -m "automated commit message";
+    git push;
+    git checkout $current_branch;
 }
 
 # Python 
 pipi() {
-	echo "Using $(pip --version)"
-	pip install --user $(./dotfiles/python/list_pypi.py | fzf --prompt 'install-package > ')
+    echo "Using $(pip --version)"
+    pip install $($HOME/dotfiles/python/list_pypi.py | fzf --prompt 'install-package > ')
 }
 
 aws() {	if [[ "$*" == *tree* ]]; then aws_s3_tree $@; else command aws $@; fi; }
