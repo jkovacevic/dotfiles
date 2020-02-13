f0c7c6afe39ed06155ad1e50efb176471a65a2f5
jkovacevic <jk.simunic@gmail.com>
2020-02-13 20:53:32 +0100

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -88,16 +88,16 @@ ts() {
     mv $1 $1.$ct
 }
 
-sync-all() {
+fa() {
     echo "- Syncing notes"
     (cd $HOME/notes; gf;)
     echo "- Syncing dotfiles"
     (cd $HOME/dotfiles; gf;)
     echo "- Syncing shared"
-    (cd $HOME/shared; gg;)
+    (cd $HOME/shared; gf;)
 }
 
-push-all() {
+pa() {
     echo "- Pushing notes"
     (cd $HOME/notes; gg;)
     echo "- Pushing dotfiles"
@@ -106,7 +106,7 @@ push-all() {
     (cd $HOME/shared; gg;)
 }
 
-status-all() {
+sa() {
     echo "- Status notes"
     (cd $HOME/notes; gss;)
     echo "- Status dotfiles"
