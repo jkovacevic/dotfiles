fcf3e3d9554a3a3bdbc05434896eef28183f7a23
jkovacevic <janko.kovacevic@smaato.com>
2020-02-18 10:18:54 +0100

    automated commit message

--- python/list_pypi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from lxml import html
-import requests
-response = requests.get("https://pypi.org/simple/")
-tree = html.fromstring(response.content)
-package_list = [package for package in tree.xpath('//a/text()')]
-for package in package_list:
-	print(package)
