da4e2eceac25efdce9bed8f5a6a6cce1bf83808f
jkovacevic <jk.simunic@gmail.com>
2020-02-27 00:09:58 +0100

    automated commit message

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,3 @@
-alias l='ls -lFh --color=auto --group-directories-first'
 alias ls='ls --color=auto --group-directories-first'
 alias lt='ls -lhrt --color=auto --group-directories-first'
 alias ldot='ls -ld .*'
