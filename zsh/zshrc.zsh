f906ebbf36cf9a7190f97a5ee0b9fba8fbd8361b
jkovacevic <janko.kovacevic@smaato.com>
2020-03-09 11:40:55 +0100

    automated commit message

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -69,7 +69,8 @@ eval "$(ntfy shell-integration)"
 
 aws-prod() { alias aws='aws --profile=smaato-prod-admin' }
 aws-test() { alias aws='aws --profile=smaato-test-admin' }
-aws-ssh() {ssh -i ~/.ssh/smart-platform-test.pem ec2-user@$1}
+aws-ssh-test() { ssh -i ~/.ssh/smart-platform-test.pem ec2-user@$1}
+aws-ssh-prod() { ssh -i ~/.ssh/smart-platform.pem ec2-user@$1}
 foxy-proxy() { ssh -i ~/.ssh/smart-platform-test.pem -N -D 8157 hadoop@$1 }
 
 function setup_tmux {
