1a69c2c4402328fc38c405fd6fc1f2f96df7688e
jkovacevic <janko.kovacevic@smaato.com>
2020-04-27 10:23:02 +0200

    automated commit message

--- sublime/User/multiselect_to_line.py
+++ sublime/User/multiselect_to_line.py
@@ -1,3 +1,4 @@
+import os
 import sublime
 import sublime_plugin
 sublime.log_commands(True)
@@ -5,15 +6,18 @@ sublime.log_commands(True)
 
 class MultiselectToLineCommand(sublime_plugin.TextCommand):
 
-    def on_done(self, content):
+    def run(self, edit):
         (row,col) = self.view.rowcol(self.view.sel()[0].begin())
+        os.environ['_MULTISELECT_TO_LINE'] = str(row)
+
+class MultiselectToLineAfterCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        (row,col) = self.view.rowcol(self.view.sel()[0].end())
         start_row = row
-        target_row = int(content)
+        target_row = int(os.environ['_MULTISELECT_TO_LINE'])
         print(start_row, target_row)
 
         region = sublime.Region(self.view.text_point(start_row, 0), self.view.text_point(target_row - 1, 0))        
         self.view.sel().clear()
         self.view.sel().add(self.view.line(region))
-
-    def run(self, edit):
-        sublime.active_window().show_input_panel("sel line:", "", self.on_done, None, None)
