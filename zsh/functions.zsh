24fb1f5a71617cdb973feacdbfde72207d49c861
jkovacevic <jk.simunic@gmail.com>
2020-05-03 17:24:36 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -102,11 +102,11 @@ gsa() {
 
 gpa() {
     echo "- Pushing notes"
-    (cd $HOME/notes; if ! gs | grep -c "Your branch is up to date"; then gg; fi;)
+    (cd $HOME/notes; if gs | grep -q -E "Changes not staged for commit|Untracked files"; then gg; fi;)
     echo "- Pushing dotfiles"
-    (cd $HOME/dotfiles; if ! gs | grep -c "Your branch is up to date"; then gg; fi;)
+    (cd $HOME/dotfiles; if gs | grep -q -E "Changes not staged for commit|Untracked files"; then gg; fi;)
     echo "- Pushing dotshared"
-    (cd $HOME/dotshared; if ! gs | grep -c "Your branch is up to date"; then gg; fi;)
+    (cd $HOME/dotshared; if gs | grep -q -E "Changes not staged for commit|Untracked files"; then gg; fi;)
 }
 
 gfa() {
