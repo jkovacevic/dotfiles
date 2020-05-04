f195d8759bba939cde62c0651791d9893b24bfa2
jkovacevic <janko.kovacevic@smaato.com>
2020-05-04 02:59:13 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,18 @@
+# Aliases
+alias cls="clear"
+alias ls='ls --color=auto --group-directories-first'
+alias lt='ls -lhrt --color=auto --group-directories-first'
+alias ldot='ls -ld .*'
+alias grep='grep --color=auto'
+alias mocp='mocp -T rhowaldt'
+alias clip='xclip -selection clipboard'
+alias edit='cat > /tmp/_.txt; subl /tmp/_.txt'
+alias cat='bat --style=plain'
+alias tw='$HOME/dotshared/tmux/tmux.startup.shell'
+alias th='$HOME/dotshared/tmux/tmux.startup.home'
+alias lpython='$HOME/local-tmp/ipython/venv/bin/python'
+alias lpip='$HOME/local-tmp/ipython/venv/bin/pip'
+
 # Functions used as commands
 sz() { source ~/.zshrc; echo "Sourced ~/.zshrc"; }
 bg() { nohup $@ > /dev/null 2>&1 & disown }
@@ -11,11 +26,45 @@ fd() { eval subl --command \'sbs_compare_files {\"A\":\"$(realpath $1)\", \"B\":
 cpcat() { cat $1 | xclip -selection clipboard; }
 cppsh() { xclip -selection clipboard -o > $1; }
 cpth() { readlink -f $1 | xargs echo -n | xclip -selection clipboard; }
-ws() { rgrep -n $@ --ignore-case --color=auto; }
+ws() { rg -n $@ --ignore-case --color=auto; }
 wss() { sudo rg -n $@ --ignore-case --color=auto; }
 ymp3() { youtube-dl --extract-audio --audio-format mp3 $1; }
 yvid() { youtube-dl $1; }
 venv() { if [[ "$VIRTUAL_ENV" == "" ]]; then source venv/bin/activate; else deactivate; fi; }
+log() { echo "$1" && notify-send "$1"; }
+ts() { ct=$(date +"%Y%m%d_%H%M%S"); mv $1 $1.$ct }
+
+short-url() {
+    urlencode() {
+      python -c 'import urllib, sys; print(urllib.quote(sys.argv[1], sys.argv[2]))' "$1" "$urlencode_safe"
+    }
+
+    encoded=$(urlencode "$1")
+    local url=$(curl --silent "https://is.gd/create.php?format=simple&url=$encoded")
+    xclip -selection clipboard <<< $url
+    echo $url
+}
+
+extract () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)        tar xjf $1        ;;
+            *.tar.gz)         tar xzf $1        ;;
+            *.bz2)            bunzip2 $1        ;;
+            *.rar)            unrar x $1        ;;
+            *.gz)             gunzip $1         ;;
+            *.tar)            tar xf $1         ;;
+            *.tbz2)           tar xjf $1        ;;
+            *.tgz)            tar xzf $1        ;;
+            *.zip)            unzip $1          ;;
+            *.Z)              uncompress $1     ;;
+            *.7z)             7zr e $1          ;;
+            *)                echo "'$1' cannot be extracted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
 
 # Git functions
 gg () { git add .; git commit -m "automated commit message"; git push; }
@@ -25,6 +74,7 @@ gf () { git pull }
 gs () { git status }
 gss () { git status --short }
 gr () { git checkout $1 }
+grh () { git reset --hard origin/$(git branch | awk '{print $NF}') }
 gh () { smerge log $1 }
 gc () { git commit -m "$1" }
 gd () { git diff $1; }
@@ -67,11 +117,11 @@ gsa() {
 
 gpa() {
     echo "- Pushing notes"
-    (cd $HOME/notes; gg;)
+    (cd $HOME/notes; if gs | grep -q -E "Changes not staged for commit|Untracked files"; then gg; fi;)
     echo "- Pushing dotfiles"
-    (cd $HOME/dotfiles; gg;)
+    (cd $HOME/dotfiles; if gs | grep -q -E "Changes not staged for commit|Untracked files"; then gg; fi;)
     echo "- Pushing dotshared"
-    (cd $HOME/dotshared; gg;)
+    (cd $HOME/dotshared; if gs | grep -q -E "Changes not staged for commit|Untracked files"; then gg; fi;)
 }
 
 gfa() {
@@ -83,105 +133,39 @@ gfa() {
     (cd $HOME/dotshared; gf;)
 }
 
-# Other functions
-create_tmp() {
-    file=$2
-    if [ "$#" -ne 2 ]; then 
-        tmp_file=$tmp_folder/$(head /dev/urandom | tr -dc a-z0-9 | head -c 13)
-    else
-        tmp_file=$tmp_folder/$file
-    fi;
-    if [[ $file =~ \.py$ ]]; then
-        echo "#!/home/jk/local-tmp/ipython/venv/bin/python" > $tmp_file
-    else
-        touch $tmp_file
-    fi;
-    subl $tmp_file;
-}
-
-tmp() {
-    if [ $# -eq 2 ] || [[ $1 == "local" ]]; then
-        tmp_folder="$HOME/notes/tmp";
-        if [ -z "$2" ]; then
-            tmp_file=$tmp_folder/$(head /dev/urandom | tr -dc a-z0-9 | head -c 13);
-        else
-            tmp_file=$tmp_folder/$2;
-        fi;
-    else
-        tmp_folder="/tmp";
-        if [ -z "$1" ]; then
-            tmp_file=$tmp_folder/$(head /dev/urandom | tr -dc a-z0-9 | head -c 13);
-        else
-            tmp_file=$tmp_folder/$1;
-        fi;
-    fi;
-    touch $tmp_file && subl $tmp_file;
-}
-
-log() {
-    echo "$1" && notify-send "$1";
-}
-
-short-url() {
-
-    urlencode() {
-      python -c 'import urllib, sys; print(urllib.quote(sys.argv[1], sys.argv[2]))' "$1" "$urlencode_safe"
-    }
-
-    encoded=$(urlencode "$1")
-    local url=$(curl --silent "https://is.gd/create.php?format=simple&url=$encoded")
-    xclip -selection clipboard <<< $url
-    echo $url
-}
-
-ts() {
-    ct=$(date +"%Y%m%d_%H%M%S")
-    mv $1 $1.$ct
-}
-
-extract () {
-    if [ -f $1 ] ; then
-        case $1 in
-            *.tar.bz2)        tar xjf $1        ;;
-            *.tar.gz)         tar xzf $1        ;;
-            *.bz2)            bunzip2 $1        ;;
-            *.rar)            unrar x $1        ;;
-            *.gz)             gunzip $1         ;;
-            *.tar)            tar xf $1         ;;
-            *.tbz2)           tar xjf $1        ;;
-            *.tgz)            tar xzf $1        ;;
-            *.zip)            unzip $1          ;;
-            *.Z)              uncompress $1     ;;
-            *.7z)             7zr e $1          ;;
-            *)                echo "'$1' cannot be extracted via extract()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file"
-    fi
-}
-
-killport () {
-    kill -9 `lsof -i tcp:$1 | tail -1 | awk '{ print $2;}'`
-}
-
 # ZLE commands
-go_back() {
-    cd ..; echo "";
-    zle reset-prompt;
-}; zle -N go_back
+# Remove keybinds
+bindkey -r "^[c"    # fzf-cd-widget
+bindkey -r "^d"     # delete-char-or-list
+bindkey -r "^[d"    # delete-blank-word
+bindkey -r "^T"     # fzf-file-widget
+bindkey -r "^L"     # clear-screen
 
-list_dir() {
-    echo ""; ls -lFh --color=auto --group-directories-first;
-    zle reset-prompt;
-}; zle -N list_dir
+# Terminal navigation
+bindkey     "^[[3~"     delete-char
+bindkey     "^[3;5~"    delete-char
+bindkey     "^[d"       delete-word
+bindkey     "^[[7~"     beginning-of-line
+bindkey     "^[[8~"     end-of-line
+bindkey     ";5D"       vi-backward-blank-word
+bindkey     ";5C"       .vi-forward-blank-word
+bindkey     "^Z"        undo
 
-reset_compinit() {
-    rm $HOME/.zcompdump && compinit
-}
+# Custom commands
+CAPS_LOCK="^[[1;2P"
+CTRL_L="^L"
+CTRL_D="^D"
+CTRL_F="^F"
+CTRL_BKSP="^H"
+CTRL_SP="^@"
+
+bindkey     $CTRL_L          go
+bindkey     $CAPS_LOCK       list_dir
+bindkey     $CTRL_BKSP       go_back
+bindkey     $CTRL_D          fzf-cd-widget
+bindkey     $CTRL_F          fzf-file-widget
 
 go() {
-    echo
-    ls -lh --color=auto --group-directories-first
     line=$(ls --color=auto --group-directories-first)
     sel=$(ls --color=auto --group-directories-first | /bin/cat -n | sed 's/ //g' | awk '{printf ("%s. %s\n", $1, $NF)}'| fzf --prompt='select > ')
     files=($(echo "$line" | tr ' ' '\n'))
@@ -195,9 +179,17 @@ go() {
         fi;
         if [ -f $f ]; then 
             xdotool type ${f} > /dev/null 2>&1
-            xdotool key "ctrl+a"
         fi;
     fi
     zle reset-prompt;
-
 }; zle -N go
+
+go_back() {
+    cd ..; echo "";
+    zle reset-prompt;
+}; zle -N go_back
+
+list_dir() {
+    echo ""; ls -lFh --color=auto --group-directories-first;
+    zle reset-prompt;
+}; zle -N list_dir
