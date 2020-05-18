5a9603d8d1be7c6d9c3e64aef253939afb892081
jkovacevic <janko.kovacevic@smaato.com>
2020-05-18 13:45:19 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -155,23 +155,26 @@ bindkey     "^Z"        undo
 
 # Jira commands
 jira-init() {
-    if [ ! "$#" -eq 1 ]; then echo "Expecting Jira ticket number as CLI argument"; exit 1; fi;
-    ticket_num=$1
-
-    jira_home="$HOME/Jira"
-    file_name="memo"
-    dir_name="$jira_home/$ticket_num"
 
     find_latest_name() {
         dir_name=$1
         file_name=$2
-        FILE_NAME="${dir_name}/${file_name}1"
+        n="1"
+        FILE_NAME="${dir_name}/${file_name}${n}"
         while [ -f $FILE_NAME ]
         do
             n=$((n + 1))
             FILE_NAME=${dir_name}/${file_name}${n}
         done
     }
+
+    if [ ! "$#" -eq 1 ]; then echo "Expecting Jira ticket number as CLI argument"; exit 1; fi;
+    ticket_num=$1
+    ticket_num=${ticket_num:l}
+
+    jira_home="$HOME/Jira"
+    file_name="record"
+    dir_name="$jira_home/$ticket_num"
 
     if [ ! -d $dir_name ]; then
         echo "Creating directory $dir_name"
@@ -182,7 +185,8 @@ jira-init() {
 
     if [ ! -f $FILE_NAME ]; then
         echo "Creating file: $FILE_NAME"
-        touch $FILE_NAME
+        echo "https://jira.smaato.net/browse/${ticket_num}" > $FILE_NAME
+        subl $FILE_NAME
     fi;
 }
 
