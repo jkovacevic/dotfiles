6061c187bc0a50a14823cebf9de5e6aec5e3526d
jkovacevic <janko.kovacevic@smaato.com>
2020-05-28 19:20:43 +0200

    automated commit message

--- startup.sh
+++ startup.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
-function between_strings {
+between_strings()` {
 	echo "$(sed -e "s/.*$2\(.*\)$3.*/\1/" <<< $1 | xargs)"
 }
 
-function disable_touchpad {
+disable_touchpad() {
 	in=$(xinput list | grep Synaptics)
 	id=$(between_strings "$in" "id=" "\[")
 	xinput --disable $id
 }
 
-function switch_home_end {
+switch_home_end() {
 	xmodmap -e "keycode 112 = Home"
 	xmodmap -e "keycode 117 = End"
 	xmodmap -e "keycode 110 = Prior"
 	xmodmap -e "keycode 115 = Next"
 }
 
-function revert_home_end {
+revert_home_end() {
 	xmodmap -e "keycode 112 = Prior"
 	xmodmap -e "keycode 117 = Next"
 	xmodmap -e "keycode 110 = Home"
 	xmodmap -e "keycode 115 = End"
 }
 
-function disable_capslock {
+disable_capslock() {
 	xmodmap -e 'clear Lock'
 	xmodmap -e "keycode 66 = F13"
 }
 
-function accelarate_touchpad {
+accelarate_touchpad() {
 	in=$(xinput list | grep TrackPoint)
 	id=$(between_strings "$in" "id=" "\[")
 	xinput --set-prop $id "libinput Accel Speed" 0.9
+}
+
+startup_laptop() {
+
 }
 
 disable_touchpad
 # switch_home_end
 disable_capslock
 accelarate_touchpad
-
 xset s off && xset -dpms
