33cdfad303d31e28320fddd11c26727f79a87c2f
jkovacevic <jk.simunic@gmail.com>
2020-09-03 11:08:35 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -176,3 +176,64 @@ start-wifi() {
 start-blue() {
     systemctl start bluetooth.service && blueman-applet
 }
+
+port-list() {
+    local v=$(sudo lsof -i -P -n)
+    echo $v | fzf
+}
+
+pid-list() {
+    local v=$(sudo ps -aux)
+    echo $v | fzf
+}
+
+jira-init() {
+    find_latest_name() {
+        dir_name=$1
+        file_name=$2
+        file_ext=$3
+        n="1"
+        FILE_NAME="${dir_name}/${file_name}${n}${file_ext}"
+        while [ -f $FILE_NAME ]
+        do
+            n=$((n + 1))
+            FILE_NAME="${dir_name}/${file_name}${n}${file_ext}"
+        done
+    }
+
+    if [ ! "$#" -eq 1 ]; then echo "Expecting Jira ticket number as CLI argument"; exit 1; fi;
+    ticket_num=$1
+    ticket_num=${ticket_num:l}
+
+    jira_home="$HOME/Jira"
+    file_name="item"
+    file_ext=".md"
+    dir_name="$jira_home/$ticket_num"
+
+    if [ ! -d $dir_name ]; then
+        echo "Creating directory $dir_name"
+        mkdir -p $dir_name
+    fi;
+
+    find_latest_name $dir_name $file_name $file_ext
+
+    if [ ! -f $FILE_NAME ]; then
+        echo "Creating file: $FILE_NAME"
+        echo "<Insert ticket title>" > $FILE_NAME
+        echo "https://jira.smaato.net/browse/${ticket_num}" >> $FILE_NAME
+        subl $FILE_NAME
+    fi;
+}
+
+jira-list() {
+    jira_home="$HOME/Jira"
+    file_name="item1.md"
+    dir_name="$jira_home/$ticket_num"
+
+    for f in $(ls $jira_home); do
+        fpath="$jira_home/$f/$file_name";
+        if [[ -f $fpath ]]; then
+            echo "$f - $(head -n 1 $fpath)";
+        fi;
+    done;
+}
