87cb86747c424573a650fe359ebea2566b604705
noctreik <noctreik@protonmail.ch>
2020-10-13 02:07:43 +0200

    automated commit message

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -29,8 +29,8 @@ cppsh() { xclip -selection clipboard -o > $1; }
 cpth() { readlink -f $1 | xargs echo -n | xclip -selection clipboard; }
 ws() { rg -n $@ --ignore-case --color=auto; }
 wss() { sudo rg -n $@ --ignore-case --color=auto; }
-ymp3() { $HOME/ipython/venv/bin/youtube-dl --extract-audio --audio-format mp3 $1; }
-yvid() { $HOME/ipython/venv/bin/youtube-dl $1; }
+ymp3() { $HOME/ipython/venv/bin/youtube-dl --extract-audio --audio-format mp3 "$1"; }
+yvid() { $HOME/ipython/venv/bin/youtube-dl "$1"; }
 venv() { if [[ "$VIRTUAL_ENV" == "" ]]; then source venv/bin/activate; else deactivate; fi; }
 log() { echo "$1" && notify-send "$1"; }
 ts() { ct=$(date +"%Y%m%d_%H%M%S"); mv $1 $1.$ct }
