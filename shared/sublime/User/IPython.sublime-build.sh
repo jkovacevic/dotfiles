b07c4ee83c489fc8bf2803f64cf9e6744e92e327
noctreik <noctreik@protonmail.ch>
2020-12-17 23:46:30 +0100

    automated commit message

--- shared/sublime/User/IPython.sublime-build.sh
+++ shared/sublime/User/IPython.sublime-build.sh
@@ -1,2 +1,2 @@
 pkill -f "urxvt -hold -title IPython";
-urxvt -hold -title "IPython" -e /usr/bin/zsh -c "source /home/jk/ipython/venv/bin/activate && ipython -i $1";
+urxvt -hold -title "IPython" -e /usr/bin/zsh -c "source $HOME/ipython/venv/bin/activate && ipython -i $1";
